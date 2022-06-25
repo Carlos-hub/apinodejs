1a43880f023879c2a40fa283ff1a8f5378fe5f5f
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-06-25 19:37:10 -0300

    iniciando conceitos de migrations

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,6 +12,7 @@ const categoriesRoutes = Router();
 
 
 categoriesRoutes.post("/",(req,res) =>{
+    console.log("reload funcionando");
    return createCategoryController.handle(req,res)
 });
 
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,11 @@ import swaggerUI from "swagger-ui-express"
 const app = express();
 import swaggerfile from "./swagger.json"
 
-
+import './database'
 app.use(express.json())
 
 app.use("/api-docs",swaggerUI.serve, swaggerUI.setup(swaggerfile))
 app.use(router);
 
 
-app.listen(3000, () => console.log("server is running"))
+app.listen(3333, () => console.log("server is running"))
